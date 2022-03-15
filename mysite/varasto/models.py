cb737659ee37ba8bc30b6e657a0510ab1052bde7
janlevea-git <janlevea@gmail.com>
2022-03-15 11:45:49 +0200

    Lisää varastoapp

---
+++ mysite/varasto/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+''' IN_DEV_PROGRESS
+# Create your models here.
+class Opiskelijat(models.Model):
+    opiskelijanro = models.IntegerField(primary_key=True)
+
+class Lainaukset(models.Model):
+    opiskelijanro = models.ForeignKey(Opiskelijat, on_delete=models.)
+    lainausaika = models.DateTimeField()
+'''
