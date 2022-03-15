bff98a59dad4713c76d22f2c8916a95f0e0dd520
janlevea-git <janlevea@gmail.com>
2022-03-15 14:16:34 +0200

    Lisää varasto.models

--- mysite/varasto/models.py
+++ mysite/varasto/models.py
@@ -1,11 +1,25 @@
 from django.db import models
 
-''' IN_DEV_PROGRESS
-# Create your models here.
 class Opiskelijat(models.Model):
     opiskelijanro = models.IntegerField(primary_key=True)
+    etunimi = models.CharField(max_length=50)
+    sukunimi = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"Opiskelija: {self.opiskelijanro} {self.etunimi} {self.sukunimi}"
+
+class Tyokalut(models.Model):
+    id = models.IntegerField(primary_key=True)
+    nimi = models.CharField(max_length=60)
+
+    def __str__(self):
+        return f"Työkalu: {self.tyokalunro} {self.nimi}"
 
 class Lainaukset(models.Model):
-    opiskelijanro = models.ForeignKey(Opiskelijat, on_delete=models.)
+    id = models.IntegerField(primary_key=True)
+    opiskelija = models.ForeignKey(Opiskelijat, on_delete=models.PROTECT)
+    tyokalu = models.ForeignKey(Tyokalut, on_delete=models.PROTECT)
     lainausaika = models.DateTimeField()
-'''
+
+    def __str__(self):
+        return f"Lainaus {self.id}, Opiskelija {self.opiskelijanro}, Työkalu {self.tyokalunro}"
