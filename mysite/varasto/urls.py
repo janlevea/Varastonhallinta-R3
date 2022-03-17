937aac5e2a75eae725bab01cfc8f3298325dcd1a
janlevea-git <janlevea@gmail.com>
2022-03-17 23:24:22 +0200

    Lisää urleja

--- mysite/varasto/urls.py
+++ mysite/varasto/urls.py
@@ -2,6 +2,14 @@ from django.urls import path
 
 from . import views
 
+app_name = "lainaukset"
 urlpatterns = [
-    path('', views.index, name='index'),
+    # /varasto/
+    path('', views.index, name="index"), 
+    # varasto/1/
+    path("<int:lainaus_id>/", views.lainaus, name="lainaus"),
+    # varasto/lainaukset/
+    path("lainaukset/", views.lainaukset, name="lainaukset"),
+    # varasto/tyokalut/
+    path("tyokalut/", views.tyokalut, name="tyokalut")
 ]
