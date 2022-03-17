4b2f2a245a50c455cdbafca97494ea346c907760
janlevea-git <janlevea@gmail.com>
2022-03-17 23:22:50 +0200

    Opiskelija, Tyokalu ja Lainaus adminpaneeliin

--- mysite/varasto/admin.py
+++ mysite/varasto/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Opiskelija, Tyokalu, Lainaus
+
+admin.site.register(Opiskelija)
+admin.site.register(Tyokalu)
+admin.site.register(Lainaus)
