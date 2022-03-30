6cc38a5c53f27c17b3b399245e9ae19154231415
janlevea-git <janlevea@gmail.com>
2022-03-30 14:25:39 +0300

    Django-progress

--- mysite/varasto/urls.py
+++ mysite/varasto/urls.py
@@ -5,11 +5,13 @@ from . import views
 app_name = "lainaukset"
 urlpatterns = [
     # /varasto/
-    path('', views.index, name="index"), 
+    path('', views.IndexView.as_view(), name="index"), 
     # varasto/1/
-    path("<int:lainaus_id>/", views.lainaus, name="lainaus"),
+    path("<int:pk>/", views.LainausView.as_view(), name="lainaus"),
+    # varasto/1/poista/
+    #path("<int:lainaus_id>/poista/", views.poista, name="poista"),
     # varasto/lainaukset/
-    path("lainaukset/", views.lainaukset, name="lainaukset"),
+    #path("lainaukset/", views.LainauksetView.as_view(), name="lainaukset"),
     # varasto/tyokalut/
-    path("tyokalut/", views.tyokalut, name="tyokalut")
+    #path("tyokalut/", views.TyokalutView.as_view(), name="tyokalut")
 ]
--- mysite/varasto/views.py
+++ mysite/varasto/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 
 from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
 
 from .models import Lainaus
+
+class IndexView(generic.ListView):
+    template_name = 'varasto/index.html'
+    context_object_name = "viimeisimmat_lainaukset"
+    
+    def get_queryset(self):
+        # Return last 5 tool loans
+        return Lainaus.objects.order_by('-lainausaika')
+
+class LainausView(generic.DetailView):
+    model = Lainaus
+    template_name = "varasto/lainaus.html"    
+
+'''
 
 # /varasto/ etusivu
 def index(request):
@@ -19,6 +35,11 @@ def lainaus(request, lainaus_id):
     laina = get_object_or_404(Lainaus, pk=lainaus_id)
     return render(request, "varasto/lainaus.html", {"laina": laina})
 
+# def poista(request, lainaus_id):
+#     laina = get_object_or_404(Lainaus, pk=lainaus_id)
+#     laina.delete(lainaus_id)
+#     return HttpResponse("Lainaus", lainaus_id, "poistettu.")
+
 def lainaukset(request):
     return HttpResponse(f"WORK IN PROGRESS")
 
@@ -28,3 +49,5 @@ def tyokalut(request):
 # WIP
 # Lainaukset tänään ... Lainaukset tällä viikolla ... 
 # Vanhat lainaukset ... Lisää lainaus ... Lisää työkalu
+
+'''
