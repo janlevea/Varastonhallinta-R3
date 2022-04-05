d8b4bda84dfbe154ec889623e52fa30106a65d92
janlevea-git <janlevea@gmail.com>
2022-04-05 09:45:29 +0300

    Muuta: djangoprojektin nimi

--- varastonhallinta/varasto/models.py
+++ varastonhallinta/varasto/models.py
@@ -1,7 +1,7 @@
 from django.db import models
-
 # from django.utils.translation import gettext_lazy as _
 
+# Create your models here.
 class Varastotyyppi(models.Model):
     id = models.AutoField(primary_key=True, null=False)
     nimi = models.CharField(max_length=30, null=False)
