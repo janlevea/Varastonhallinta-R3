b20733e9d28dd4f48c002bb617acd6b32f28b67f
janlevea-git <janlevea@gmail.com>
2022-04-06 14:12:13 +0300

    Tee: lainausformi suoraan modelista

--- varastonhallinta/varasto/forms.py
+++ varastonhallinta/varasto/forms.py
@@ -1,9 +1,19 @@
 from django import forms
+from varasto.models import Varasto, Tuoteryhma, Tuote, Varastotapahtuma
 
-class UusiLainaus(forms.Form):
-    varastonhoitaja = forms.CharField(label="Varastonhoitaja", max_length=35, required=True)
-    asiakas = forms.CharField(label="Asiakas", max_length=35, required=True)
-    tuote = forms.CharField(label="Tuote", max_length=35, required=True)
-    maara = forms.IntegerField(label="Määrä", required=True) # help_text="Kuinka monta?"
-    aikaleima = forms.DateField(label="Aikaleima", required=True)
-    palautuspaiva = forms.DateField(label="Palautuspäivä", required=True)
+# Modelista tehty formi
+class UusiLainaus(forms.ModelForm):
+    class Meta:
+        model = Varastotapahtuma
+        fields = [
+        "varastonhoitaja", "asiakas", 
+        "tuote", "maara", "palautuspaiva"]
+
+# Käsin tehty formi
+# class UusiLainaus(forms.Form):
+#     varastonhoitaja = forms.CharField(label="Varastonhoitaja", max_length=35, required=True)
+#     asiakas = forms.CharField(label="Asiakas", max_length=35, required=True)
+#     tuote = forms.CharField(label="Tuote", max_length=35, required=True)
+#     maara = forms.IntegerField(label="Määrä", required=True) # help_text="Kuinka monta?"
+#     aikaleima = forms.DateField(label="Aikaleima", required=True)
+#     palautuspaiva = forms.DateField(label="Palautuspäivä", required=True)
