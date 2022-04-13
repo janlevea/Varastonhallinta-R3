124345bf967930399d5322edb208241a4d494153
janlevea-git <janlevea@gmail.com>
2022-04-13 10:01:15 +0300

    Lisää: Linkkeihin osoite/varasto/.../

--- varastonhallinta/varasto/models.py
+++ varastonhallinta/varasto/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # from django.utils.translation import gettext_lazy as _
+from django.urls import reverse
 
 # Importoi date/time-kirjastot käytettäväksi aikaleimoissa/palautuspäivässä
 from datetime import timedelta
--- varastonhallinta/varastonhallinta/urls.py
+++ varastonhallinta/varastonhallinta/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+#from django.http import HttpResponseRedirect
+from varastonhallinta.views import index
+
 urlpatterns = [
-    path('', include('varasto.urls')),
+    path('', index),
+    path('varasto/', include('varasto.urls')),
     path('admin/', admin.site.urls),
 ]
