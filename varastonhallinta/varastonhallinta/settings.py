d1a8eea9d97d90d0f990fef0dacb60de2ad521e5
janlevea-git <janlevea@gmail.com>
2022-04-19 10:08:01 +0300

    Luo: uusi secret_key ja piilota se repositoriosta

--- varastonhallinta/varastonhallinta/settings.py
+++ varastonhallinta/varastonhallinta/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+# secret key .env tiedostosta
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$t@(b_s7=8g9opjo9)yu7c&u9enwp5tlzklt@s!eyt^q!o-q3$'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
