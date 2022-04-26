a225270812cacb633e35a7163125b159d079014c
janlevea <janlevea@gmail.com>
2022-04-26 13:37:06 +0300

    Kommentointia

--- varastonhallinta/kayttajat/urls.py
+++ varastonhallinta/kayttajat/urls.py
@@ -5,8 +5,12 @@ import django.contrib.auth.urls
 app_name = "kayttajat"
 
 urlpatterns = [
+    # kayttajat/auth_urls
     path("", include(django.contrib.auth.urls)),
+    # kayttajat/profiili/<opiskelijanumero>/
     path("profiili/<str:opiskelijanumero>/", views.profiili, name="profiili"),
+    # kayttajat/rekisteroidy/
     path("rekisteroidy/", views.rekisteroidy, name="rekisteroidy"),
+    # kayttajat/lista/
     path("lista/", views.kayttajalista, name="kayttajalista"),
 ]
