6ae6d5a57c2b84049a9b4d325fdb0b3e07ba9e30
janlevea <janlevea@gmail.com>
2022-04-27 12:36:40 +0300

    Varastotapahtuma - adminsivun kustomointi

--- varastonhallinta/varasto/admin.py
+++ varastonhallinta/varasto/admin.py
@@ -3,4 +3,35 @@ from django.contrib import admin
 # Importoi modelit ja lisää ne näkymään admin-sivulla
 from .models import Varastotapahtuma
 
-admin.site.register(Varastotapahtuma)
+# Importoi date/time-kirjastot käytettäväksi aikaleimoissa/palautuspäivässä
+from datetime import timedelta
+from django.utils import timezone
+
+# UUID arkistotunnuksen luontia varten
+import uuid
+
+class VarastotapahtumaAdmin(admin.ModelAdmin):
+    fields = (
+        "arkistotunnus",
+        "tuote", "maara",
+        "palautuspaiva",
+        "asiakas", "varastonhoitaja"
+    )
+
+    readonly_fields = ["arkistotunnus"]
+
+    def get_changeform_initial_data(self, request):
+        defPalautuspaiva = (timezone.now() + timedelta(days=14))
+        return {
+            "palautuspaiva": defPalautuspaiva,
+            "varastonhoitaja": request.user
+        }
+
+    def save_model(self, request, obj, form, change):
+        obj.arkistotunnus = uuid.uuid1()
+        obj.varastonhoitaja = request.user
+        super().save_model(request, obj, form, change)
+
+admin.site.register(Varastotapahtuma, VarastotapahtumaAdmin)
+
+# TODO: Arkistotunnus kenttä pois varastotapahtuman lisäysnäkymästä
