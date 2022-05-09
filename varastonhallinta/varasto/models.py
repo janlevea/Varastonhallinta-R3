05a72f83854747bb7374f875ab769854f00e1d9f
Tuomas Suutari <tuomas@nepnep.net>
2022-05-09 14:42:10 +0300

    Yksinkertaistusta ja QuerySet-laajennos

--- varastonhallinta/varasto/models.py
+++ varastonhallinta/varasto/models.py
@@ -7,8 +7,23 @@ from tuotteet.models import Tuote
 # UUID arkistotunnuksen luontia varten
 import uuid
 
-class VarastotapahtumaBase(models.Model):
+
+# Mahdollista seuraava filtteröinti:
+#    Varastotapahtuma.objects.avoimet()
+#  ja
+#    Varastotapahtuma.objects.vanhat()
+class VarastotapahtumaQuerySet(models.QuerySet):
+    def avoimet(self):
+        return self.filter(avoin=True)
+
+    def vanhat(self):
+        return self.filter(avoin=False)
+
+
+class Varastotapahtuma(models.Model):
     id = models.AutoField(primary_key=True, null=False)
+
+    avoin = models.BooleanField(default=True)
 
     arkistotunnus = models.UUIDField(
         default=uuid.uuid4, editable=False, null=False, blank=False, 
@@ -27,24 +42,6 @@ class VarastotapahtumaBase(models.Model):
         Kayttaja, null=False, on_delete=models.PROTECT, verbose_name="Asiakas",  related_name="asiakas")
     varastonhoitaja = models.ForeignKey(
         Kayttaja, null=False, on_delete=models.PROTECT, verbose_name="Varastonhoitaja", related_name="varastonhoitaja")
-    
-    class Meta:
-        verbose_name = "Varastotapahtuma"
-        verbose_name_plural = "Varastotapahtumat"
-        abstract = True
-        ordering = ['asiakas']
-    def __str__(self):
-        return f"id({self.id}) {self.tuote.nimike} {self.maara}kpl, Asiakas: {self.asiakas}, Varastonhoitaja: {self.varastonhoitaja}"
-
-class Varastotapahtuma(VarastotapahtumaBase):
-    pass
-
-class VarastotapahtumaOld(VarastotapahtumaBase):
-    asiakas = models.ForeignKey(
-        Kayttaja, null=False, on_delete=models.PROTECT, verbose_name="Asiakas",  related_name="asiakas_oli")
-
-    varastonhoitaja = models.ForeignKey(
-        Kayttaja, null=False, on_delete=models.PROTECT, verbose_name="Varastonhoitaja", related_name="varastonhoitaja_oli")
 
     palautettu = models.DateTimeField(
         blank=True, null=True, editable=False, verbose_name="Palautettu")
@@ -55,6 +52,13 @@ class VarastotapahtumaOld(VarastotapahtumaBase):
     varastonhoitaja_poisto_palautus = models.ForeignKey(
         Kayttaja, null=False, on_delete=models.PROTECT, verbose_name="Varastonhoitaja poisto/palautus",  related_name="varastonhoitaja_poisto_palautus")
 
-    class Meta(VarastotapahtumaBase.Meta):
-        verbose_name = "Vanha varastotapahtuma"
-        verbose_name_plural = "Vanhat varastotapahtumat"
+    objects = VarastotapahtumaQuerySet.as_manager()
+    
+    class Meta:
+        verbose_name = "Varastotapahtuma"
+        verbose_name_plural = "Varastotapahtumat"
+        ordering = ['asiakas']
+
+   def __str__(self):
+        return f"id({self.id}) {self.tuote.nimike} {self.maara}kpl, Asiakas: {self.asiakas}, Varastonhoitaja: {self.varastonhoitaja}"
+
