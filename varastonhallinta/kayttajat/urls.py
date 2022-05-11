7d1950a16dfe0d22aba9baedad77ee893dca2bc4
janlevea <janlevea@gmail.com>
2022-05-11 12:30:33 +0300

    Lisää: Tyhjiä rivejä (siistimistä)

--- varastonhallinta/kayttajat/urls.py
+++ varastonhallinta/kayttajat/urls.py
@@ -7,10 +7,13 @@ app_name = "kayttajat"
 urlpatterns = [
     # kayttajat/auth_urls
     path("", include(django.contrib.auth.urls)),
+    
     # kayttajat/profiili/<opiskelijanumero>/
     path("profiili/<str:opiskelijanumero>/", views.profiili, name="profiili"),
+
     # kayttajat/rekisteroidy/
     path("rekisteroidy/", views.rekisteroidy, name="rekisteroidy"),
+
     # kayttajat/lista/
     path("lista/", views.kayttajalista, name="kayttajalista"),
 ]
