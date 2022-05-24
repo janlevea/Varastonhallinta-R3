de264c2962cba705b0227c5c215b3a312263fd0d
janlevea <janlevea@gmail.com>
2022-05-24 12:40:04 +0300

    Lisää: AktiivinenFilter

--- varastonhallinta/kayttajat/admin.py
+++ varastonhallinta/kayttajat/admin.py
@@ -1,11 +1,43 @@
 from django.contrib import admin
 
+from django.contrib.admin import SimpleListFilter
+from django.utils.translation import gettext_lazy as _
+
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth import get_user_model
 from .forms import UserAdminCreationForm, UserAdminChangeForm
 # from django.contrib.auth.models import Group
 
 Kayttaja = get_user_model()
+
+class AktiivinenFilter(SimpleListFilter):
+    title = _('Aktiivinen')
+    parameter_name = 'aktiivinen'
+
+    def lookups(self, request, model_admin):
+        return (
+            (None, _("Kyllä")),
+            ("ei", _("Ei")),
+            ("kaikki", _("Kaikki"))
+        )
+
+    def choices(self, cl):
+        for (lookup, title) in self.lookup_choices:
+            yield {
+                'selected': self.value() == lookup,
+                'query_string': cl.get_query_string({
+                    self.parameter_name: lookup,
+                }, []),
+                'display': title,
+            }
+
+    def queryset(self, request, queryset):
+        value = self.value()
+        if value is None:
+            return queryset.filter(aktiivinen=True)
+        elif value == 'ei':
+            return queryset.filter(aktiivinen=False)
+        return queryset
 
 class UserAdmin(BaseUserAdmin):
     # The forms to add and change user instances
@@ -17,7 +49,7 @@ class UserAdmin(BaseUserAdmin):
     # These override the definitions on the base UserAdmin
     # that reference specific fields on auth.User.
     list_display = ['opiskelijanumero', 'sukunimi', 'etunimi', 'admin']
-    list_filter = ['admin', 'staff', 'aktiivinen']
+    list_filter = ['admin', 'staff', AktiivinenFilter]
 
     fieldsets = (
         (None, {'fields': ('email', 'password', 'liittynyt')}),
