73d78c43dd64cabef7ad565926b5f6fa391891c3
janlevea <janlevea@gmail.com>
2022-05-24 12:40:17 +0300

    Lisää: PoistettuFilter

--- varastonhallinta/tuotteet/admin.py
+++ varastonhallinta/tuotteet/admin.py
@@ -1,9 +1,41 @@
 from django.contrib import admin
 
+from django.contrib.admin import SimpleListFilter
+from django.utils.translation import gettext_lazy as _
+
 from .models import Tuote, Tuoteryhma
 # from varasto.admin import ReadOnlyAdminMixin
 
 from stringToBCode import string2barcode
+
+class PoistettuFilter(SimpleListFilter):
+    title = _('Poistettu')
+    parameter_name = 'poistettu'
+
+    def lookups(self, request, model_admin):
+        return (
+            (None, _("Ei")),
+            ("kylla", _("Kyllä")),
+            ("kaikki", _("Kaikki"))
+        )
+
+    def choices(self, cl):
+        for (lookup, title) in self.lookup_choices:
+            yield {
+                'selected': self.value() == lookup,
+                'query_string': cl.get_query_string({
+                    self.parameter_name: lookup,
+                }, []),
+                'display': title,
+            }
+
+    def queryset(self, request, queryset):
+        value = self.value()
+        if value is None:
+            return queryset.filter(poistettu=False)
+        elif value == 'kylla':
+            return queryset.filter(poistettu=True)
+        return queryset
 
 class TuoteryhmaAdmin(admin.ModelAdmin):
     fields = (
@@ -15,10 +47,9 @@ class TuoteryhmaAdmin(admin.ModelAdmin):
     list_display = (
         "nimi", "id", "poistettu", "lisaaja", "lisaysaika"
     )
-    list_filter = ("poistettu",)
+    list_filter = [PoistettuFilter]
     ordering = ["nimi"]
     
-# TODO: Suljetut tuoteryhmät erikseen
 # TODO: Tuotemäärä näkyviin tuoteryhmissä
 
 class TuoteAdmin(admin.ModelAdmin):
@@ -34,14 +65,12 @@ class TuoteAdmin(admin.ModelAdmin):
     list_display = (
         "nimike", "id", "tuoteryhma", "maara", "hankintapaikka", "poistettu"
     )
-    list_filter =  ("poistettu",)
+    list_filter = [PoistettuFilter]
     ordering = ["nimike"]
 
     def save_model(self, request, obj, form, change):
         obj.viivakoodi_encoded = string2barcode(obj.viivakoodi_plaintxt, codeType="B")
         super().save_model(request, obj, form, change)
-
-# TODO: Poistetut tuotteet erikseen
 
 admin.site.register(Tuoteryhma, TuoteryhmaAdmin)
 admin.site.register(Tuote, TuoteAdmin)
